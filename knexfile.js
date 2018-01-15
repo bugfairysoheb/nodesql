92fd05a55bcea7025ca5716f4fc8ffd0bc2c2972
soheb <>
2018-01-15 11:30:02 -0800

    Login functionality added

--- knexfile.js
+++ knexfile.js
@@ -5,4 +5,15 @@ module.exports = {
     password: '',
     database: 'tutorial_node_database'
   }
-}
+}	
+
+// const knex = require('knex')(require('./knexfile'))
+// module.exports = {
+//   createUser ({ username, password }) {
+//     console.log(`Add user ${username} with password ${password}`)
+//     return knex('user').insert({
+//       username,
+//       password
+//     })
+//   }
+// }
--- public/app.js
+++ public/app.js
@@ -1,3 +1,23 @@
+// const CreateUser = document.querySelector('.CreateUser')
+// CreateUser.addEventListener('submit', (e) => {
+//   e.preventDefault()
+//   const username = CreateUser.querySelector('.username').value
+//   const password = CreateUser.querySelector('.password').value
+//   post('/createUser', { username, password })
+// })
+// function post (path, data) {
+//   return window.fetch(path, {
+//     method: 'POST',
+//     headers: {
+//       'Accept': 'application/json',
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(data)
+//   })
+// }
+
+// Adding Logic to check authentication ------------------------------
+
 const CreateUser = document.querySelector('.CreateUser')
 CreateUser.addEventListener('submit', (e) => {
   e.preventDefault()
@@ -5,6 +25,18 @@ CreateUser.addEventListener('submit', (e) => {
   const password = CreateUser.querySelector('.password').value
   post('/createUser', { username, password })
 })
+const Login = document.querySelector('.Login')
+Login.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const username = Login.querySelector('.username').value
+  const password = Login.querySelector('.password').value
+  post('/login', { username, password })
+    .then(({ status }) => {
+      if (status === 200) alert('login success')
+      else alert('login failed')
+    })
+})
+
 function post (path, data) {
   return window.fetch(path, {
     method: 'POST',
